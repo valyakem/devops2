4283831f7e4228228337356b296e5e1fb18d61e6
valyakem <valyakem@yahoo.co.uk>
2022-11-05 13:59:33 -0700

    modified calculator .py file

--- calculator.py
+++ calculator.py
@@ -1,8 +1,12 @@
+
 def add(first_term, second_term):
+
     return first_term + second_term
 
 def multiply(first_term, second_term):
+
     return first_term * second_term
 
 def subtract(first_term, second_term):
+    
     return first_term - second_term
