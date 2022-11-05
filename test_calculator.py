e2153cf02816d84be6a3b2007037100aa7fd8f4d
valyakem <valyakem@yahoo.co.uk>
2022-11-05 12:22:58 -0700

    first push

---
+++ test_calculator.py
@@ -0,0 +1,19 @@
+"""
+Unit tes for the calculator library
+"""
+
+import calculator
+
+class TestCalculator:
+    def test_addition(self):
+        assert 4 == calculator.add(2,2)
+
+    def test_addition2(self):
+        assert 5 == calculator.add(2,3)
+    
+    def test_subtraction(self):
+        assert 2 == calculator.subtract(2,2)
+    
+    def test_multiply(self):
+        assert 100 == calculator.multiply(10,10)
+    
