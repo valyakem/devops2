63d394a94823467ca28032da00529344fa895cba
valyakem <valyakem@yahoo.co.uk>
2022-11-05 14:02:48 -0700

    updated file

--- calculator.py
+++ calculator.py
@@ -1,12 +1,12 @@
 
-def add(first_term, second_term):
 
+def add(first_term, second_term):
     return first_term + second_term
 
 def multiply(first_term, second_term):
-
     return first_term * second_term
 
 def subtract(first_term, second_term):
-    
     return first_term - second_term
+
+
