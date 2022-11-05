ac3cebea4b1c223eb9a1478deab0be6ef5b1d21f
valyakem <valyakem@yahoo.co.uk>
2022-11-05 13:41:41 -0700

    updated the lines

--- test_calculator.py
+++ test_calculator.py
@@ -1,19 +1,13 @@
 """
 Unit tes for the calculator library
 """
-
 import calculator
-
 class TestCalculator:
     def test_addition(self):
-        assert 4 == calculator.add(2,2)
-
+        assert 4==calculator.add(2,2)
     def test_addition2(self):
-        assert 5 == calculator.add(2,3)
-    
+        assert 5==calculator.add(2,3)
     def test_subtraction(self):
-        assert 2 == calculator.subtract(2,2)
-    
+        assert 2==calculator.subtract(2,2)
     def test_multiply(self):
-        assert 100 == calculator.multiply(10,10)
-    
+        assert 100==calculator.multiply(10,10)
