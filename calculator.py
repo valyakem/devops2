2fe1c6cb8d0340612a91e5415ed8cac9c9e0d64f
valyakem <valyakem@yahoo.co.uk>
2022-11-05 14:04:43 -0700

    udpated test case

--- calculator.py
+++ calculator.py
@@ -3,8 +3,10 @@
 def add(first_term, second_term):
     return first_term + second_term
 
+
 def multiply(first_term, second_term):
     return first_term * second_term
+
 
 def subtract(first_term, second_term):
     return first_term - second_term
